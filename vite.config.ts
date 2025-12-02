408ec904e47e515b69db7b2485053b7e8013dde5
Amit Pardeshi <amit.pardeshi2000@gmail.com>
2025-12-03 00:53:26 +0530

    update vite path

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   // ✅ Important for GitHub Pages deployment
-  base: '/portfolio/',
+  base: '/my-portfolio/',
 
   plugins: [react()],
 
